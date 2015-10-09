6483e01396a5c80304fdd1e397e88d78a0a062c9
pingu <pingu@norah>
2015-10-09 23:52:00 +0200

    added python wps decorator

---
+++ slides/birdhouse-architecture/myplot.py
@@ -0,0 +1,7 @@
+def myplot(nc_file, variable):
+    """
+    nc_file application/netcdf
+    variable string
+    """
+    # plot variable of nc_file
+    return plot.png
---
+++ slides/birdhouse-architecture/wps_myplot.py
@@ -0,0 +1,8 @@
+@wps
+def myplot(nc_file, variable):
+    """
+    nc_file application/netcdf
+    variable string
+    """
+    # plot variable of nc_file
+    return plot.png
